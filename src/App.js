e5582dacc8e7b285508253caf5f411b70dca6433
yijun <yijun@163.com>
2019-06-14 21:13:46 +0800

    add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react'
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import { BrowserRouter, Route, Switch} from 'react-router-dom'
 import Login from './pages/login/login'
 import Admin from './pages/admin/admin'
 
@@ -15,3 +15,13 @@ export default class App extends Component{
         )
     }
 }
+/* 1. 项目开发准备
+    1). 描述项目
+    2). 技术选型 
+    3). API接口/接口文档/测试接口
+    
+ 2. 启动项目开发
+    1). 使用react脚手架创建项目
+    2). 开发环境运行: npm start
+    3). 生产环境打包运行: npm run build   serve build
+     */
